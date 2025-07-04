da6bd8e5b4733f585babd6af8b268bbd61e7597f
ulxsth <ulxsth@gmail.com>
2025-07-04 18:13:53 +0900

    chore: AWS Application Composer と cdk-from-cfn のセットアップ

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export interface MyStackProps extends cdk.StackProps {
+}
+
+export class MyStack extends cdk.Stack {
+  public constructor(scope: cdk.App, id: string, props: MyStackProps = {}) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Resources
+    const dummyParameter = new ssm.CfnParameter(this, 'DummyParameter', {
+      name: '/dummy/placeholder',
+      value: 'placeholder',
+      type: 'String',
+    });
   }
 }
