d4a6687d637adef0f098611d286e26e3930fed52
sekibuuun <akaunto.no.001@gmail.com>
2025-07-09 13:38:23 +0900

    feat: state で card 情報を管理

--- frontend/src/app/admin/cards/page.tsx
+++ frontend/src/app/admin/cards/page.tsx
@@ -1,22 +1,56 @@
+"use client";
+
+import { useState } from "react";
+
+type Card = {
+  name: string;
+  cost: number;
+  attack: number;
+  hp: number;
+};
+
 export default function AdminCardsPage() {
+  const [card, setCard] = useState<Card>({
+    name: "",
+    cost: 1,
+    attack: 1,
+    hp: 1,
+  });
+
+  const handleCardNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCard({ ...card, name: e.target.value });
+  };
+
+  const handleCardCostChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCard({ ...card, cost: Number(e.target.value) });
+  };
+
+  const handleCardAttackChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCard({ ...card, attack: Number(e.target.value) });
+  };
+
+  const handleCardHpChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCard({ ...card, hp: Number(e.target.value) });
+  };
+
   return (
     <div>
       <h1>カード管理画面</h1>
       <div>
         <p>名前</p>
-        <input type="text" placeholder="名前を入力" />
+        <input type="text" placeholder="名前を入力" value={card.name} onChange={handleCardNameChange} />
       </div>
       <div>
         <p>コスト</p>
-        <input type="number" min={1} max={10} />
+        <input type="number" min={1} max={10} value={card.cost} onChange={handleCardCostChange} />
       </div>
       <div>
         <p>攻撃力</p>
-        <input type="number" min={0} max={100} />
+        <input type="number" min={1} max={100} value={card.attack} onChange={handleCardAttackChange} />
       </div>
       <div>
         <p>HP</p>
-        <input type="number" min={1} max={100} />
+        <input type="number" min={1} max={100} value={card.hp} onChange={handleCardHpChange} />
       </div>
     </div>
   );
