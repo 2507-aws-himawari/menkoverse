25ca29eff766af9cbcfbec198a28ab4f087ca80b
sekibuuun <akaunto.no.001@gmail.com>
2025-07-10 18:34:57 +0900

    fix: trpc に関連する記述の削除

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,10 @@
-import { HydrateClient } from "@/trpc/server";
 import { SignIn } from "@/app/components/signIn";
 
 export default async function Home() {
 	return (
-		<HydrateClient>
-			<div>
-				<h1>Welcome to Menkoverse</h1>
-				<SignIn />
-			</div>
-		</HydrateClient>
+		<div>
+			<h1>Welcome to Menkoverse</h1>
+			<SignIn />
+		</div>
 	);
 }
