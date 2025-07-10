986848aaa887a6253d7246619ddc0f25cc5df0c1
sekibuuun <akaunto.no.001@gmail.com>
2025-07-10 13:56:35 +0900

    fix: SignIn コンポーネントの利用

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,12 @@
-import { signIn } from "@/server/auth";
 import { HydrateClient } from "@/trpc/server";
+import { SignIn } from "@/app/components/signIn";
 
 export default async function Home() {
 	return (
 		<HydrateClient>
 			<div>
 				<h1>Welcome to Menkoverse</h1>
-				<button
-					type="button"
-					onClick={async () => {
-						"use server";
-						await signIn(undefined, { redirectTo: "/home" });
-					}}
-				>
-					ログイン
-				</button>
+				<SignIn />
 			</div>
 		</HydrateClient>
 	);
