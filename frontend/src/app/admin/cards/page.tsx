220eb539d54e45ab7c387a1905bedcb28050349c
sekibuuun <akaunto.no.001@gmail.com>
2025-07-11 13:50:15 +0900

    chore: biome check

--- frontend/src/app/admin/cards/page.tsx
+++ frontend/src/app/admin/cards/page.tsx
@@ -1,21 +1,21 @@
 "use client";
 
+import { ShowCard } from "@/app/admin/cards/_components/ShowCard";
+import { cardSchema } from "@/lib/schema/card";
+import type { Card } from "@/types/card";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { ShowCard } from "@/app/admin/cards/_components/ShowCard";
-import type { Card } from "@/types/card";
-import { cardSchema } from "@/lib/schema/card";
 
 export default function AdminCardsPage() {
 	const [isSuccess, setIsSuccess] = useState(false);
-  const [cardAttributes, setCardAttributes] = useState<Card>({
-    id: "",
-    name: "",
-    cost: 1,
-    attack: 1,
-    hp: 1,
-  });
+	const [cardAttributes, setCardAttributes] = useState<Card>({
+		id: "",
+		name: "",
+		cost: 1,
+		attack: 1,
+		hp: 1,
+	});
 
 	const {
 		register,
@@ -24,12 +24,12 @@ export default function AdminCardsPage() {
 		reset,
 	} = useForm<Card>({
 		resolver: zodResolver(cardSchema),
-    defaultValues: {
-      id: "",
-      name: "",
-      cost: 1,
-      attack: 1,
-      hp: 1,
+		defaultValues: {
+			id: "",
+			name: "",
+			cost: 1,
+			attack: 1,
+			hp: 1,
 		},
 	});
 
@@ -49,14 +49,13 @@ export default function AdminCardsPage() {
 			if (!response.ok) {
 				throw new Error("カードの作成に失敗しました");
 			}
-
-    } catch (error) {
+		} catch (error) {
 			throw new Error("カードの作成に失敗しました");
 		}
 
 		setIsSuccess(true);
-    setCardAttributes(card);
-    reset();
+		setCardAttributes(card);
+		reset();
 	};
 
 	return (
@@ -66,26 +65,47 @@ export default function AdminCardsPage() {
 				<div>
 					<p>名前</p>
 					<input type="text" placeholder="名前を入力" {...register("name")} />
-					{errors.name && <div style={{ color: "red" }}>{errors.name.message}</div>}
+					{errors.name && (
+						<div style={{ color: "red" }}>{errors.name.message}</div>
+					)}
 				</div>
 				<div>
 					<p>コスト</p>
-					<input type="number" min={1} max={10} {...register("cost", { valueAsNumber: true })} />
-					{errors.cost && <div style={{ color: "red" }}>{errors.cost.message}</div>}
+					<input
+						type="number"
+						min={1}
+						max={10}
+						{...register("cost", { valueAsNumber: true })}
+					/>
+					{errors.cost && (
+						<div style={{ color: "red" }}>{errors.cost.message}</div>
+					)}
 				</div>
 				<div>
 					<p>攻撃力</p>
-					<input type="number" min={1} max={100} {...register("attack", { valueAsNumber: true })} />
-					{errors.attack && <div style={{ color: "red" }}>{errors.attack.message}</div>}
+					<input
+						type="number"
+						min={1}
+						max={100}
+						{...register("attack", { valueAsNumber: true })}
+					/>
+					{errors.attack && (
+						<div style={{ color: "red" }}>{errors.attack.message}</div>
+					)}
 				</div>
 				<div>
 					<p>HP</p>
-					<input type="number" min={1} max={100} {...register("hp", { valueAsNumber: true })} />
+					<input
+						type="number"
+						min={1}
+						max={100}
+						{...register("hp", { valueAsNumber: true })}
+					/>
 					{errors.hp && <div style={{ color: "red" }}>{errors.hp.message}</div>}
 				</div>
 				<button type="submit">作成</button>
 			</form>
-      {isSuccess && <ShowCard cardAttributes={cardAttributes} />}
+			{isSuccess && <ShowCard cardAttributes={cardAttributes} />}
 		</div>
 	);
 }
