d434008b08c1f1accd119f6b3aa3e07096e465c6
ulxsth <ulxsth@gmail.com>
2025-07-12 17:49:13 +0900

    chore: 画像検知done

--- opencv/generate-marker.py
+++ opencv/generate-marker.py
@@ -1,30 +1,31 @@
-### arucoマーカーを生成して、画像として保存する
 import cv2
+import numpy as np
 from cv2 import aruco
-import os
 
-### --- parameter --- ###
+# サイズとオフセット値
+size = 150
+offset = 10
+x_offset = y_offset = offset // 2
 
-# マーカーの保存先
-dir_mark = r'/home/yotu/github/menkoverse/opencv/markers'
+# マーカーの個数
+num_markers = 6
+grid_size = int(np.ceil(np.sqrt(num_markers)))
 
-# 生成するマーカー用のパラメータ
-num_mark = 20 #個数
-size_mark = 500 #マーカーのサイズ
+# 辞書を取得して画像を生成
+dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 
-### --- マーカーを生成して保存する --- ###
-# マーカー種類を呼び出し
-dict_aruco = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+# 白い画像を作成（すべてのマーカーを収めるために適切なサイズにする）
+img_size = grid_size * (size + offset)
+img = np.zeros((img_size, img_size), dtype=np.uint8)
+img += 255
 
-for count in range(num_mark) :
+# マーカーを生成して画像に重ねる
+for marker_id in range(num_markers):
+    ar_img = aruco.generateImageMarker(dictionary, marker_id, size)
+    row = marker_id // grid_size
+    col = marker_id % grid_size
+    y_start = row * (size + offset) + y_offset
+    x_start = col * (size + offset) + x_offset
+    img[y_start:y_start + ar_img.shape[0], x_start:x_start + ar_img.shape[1]] = ar_img
 
-    id_mark = count #countをidとして流用
-    img_mark = aruco.generateImageMarker(dict_aruco, id_mark, size_mark)
-
-    if count < 10 :
-        img_name_mark = 'mark_id_0' + str(count) + '.jpg'
-    else :
-        img_name_mark = 'mark_id_' + str(count) + '.jpg'
-    path_mark = os.path.join(dir_mark, img_name_mark)
-
-    cv2.imwrite(path_mark, img_mark)
+cv2.imwrite("markers_0_to_5.png", img)
